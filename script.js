0a2bd62c977bce052c54937540e0baf7d72aa079
jscodin <jascode9@gmail.com>
2023-05-01 01:45:22 +0100

    Create function that plays one round of gameChange

--- script.js
+++ script.js
@@ -13,5 +13,33 @@ const pcChoice = Math.floor(Math.random() * pcList.length);
 return pcChoice, pcList[pcChoice];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
 
+  if (playerSelection === "rock" && computerSelection === "Scissors") {
+    return "Rock beats scissors, you win!";
+  }
+   else if (playerSelection === "rock" && computerSelection === "Rock") {
+    return "Oops it's a draw..";
+  }
+  else if (playerSelection === "scissors" && computerSelection === "Paper") {
+    return "Scissors beats paper, you win!";
+  }
+  else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+    return "Oops it's a draw..";
+  }
+  else if (playerSelection === "paper" && computerSelection === "Rock") {
+    return "Paper beats rock, you win!";
+  }
+  else if (playerSelection === "paper" && computerSelection === "Paper") {
+    return "Oops it's a draw..";
+  }
+  else {
+    return "Oh no, you lose this time!";
+  }
+
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
