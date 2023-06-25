b3f7aeb356c8b65c5fc3a0b70861a915a4e1674b
jscodin <jascode9@gmail.com>
2023-06-25 02:59:21 +0100

    Update comments and new function - Add comments to provide clarity on each function so far - Upload a "work in progress" score keeping function

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
+// this function gets computer to randomly select choice from the array
 function getComputerChoice() {
+
     const pcList = [
         "Rock",
         "Paper",
         "Scissors"
     ];
 
-
 const pcChoice = Math.floor(Math.random() * pcList.length);
-
 //console.log(pcList[pcChoice])
-
 return pcChoice, pcList[pcChoice];
+
 }
 
+// This function plays the game and returns the result of each potential outcome
 function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === "rock" && computerSelection === "Scissors") {
@@ -39,22 +40,44 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-
+// This allows the player to input their choice and play the game
 function game() {
 
   const computerSelection = getComputerChoice();
   console.log(computerSelection);
   let playerPrompt = prompt("Choose your weapon: ");
-  playRound(playerPrompt.toLowerCase(), computerSelection)
+  let playerScore = playRound(playerPrompt.toLowerCase(), computerSelection)
   console.log(playRound(playerPrompt, computerSelection));
+  console.log(playerScore);
+  return playerScore;
+
 }
 
+// Loops the game 5 times
 function playGame() {
 
   for (let i = 0; i < 5; i++) {
-   game();
+    game();
   }
   
+}
+
+// Still needs work, but this should keep score of both player and computer
+// My understanding of increments needs refreshing probs
+function score() {
+
+  let pScore = 0;
+  let cScore = 0;
+
+  if (playRound() === "You win!") {
+   pScore++;
+    console.log(pScore + " player");
+  }
+  else if (playRound() === "You lose!") {
+    cScore++;
+    console.log(cScore + " computer");
+  }
+
 }
 
 playGame();
@@ -62,7 +85,7 @@ playGame();
 
 /* function finalResult() {
 
-  if (playerScore > computerScore) {
+  if (> computerScore) {
     return "Well done, you win!";
   } else if (computerScore > playerScore){
     return "Unlucky, try again next time";
